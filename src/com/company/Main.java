1731da11a24f5f10b25bb4bc71636374691f32a6
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2019-10-08 12:24:02 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,41 +1,56 @@
 package com.company;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class Main {
 
     public static void main(String[] args) {
-        int n;
-        final int weight=30;
+        int n = -1;
+        final int weight = 30;
         Scanner in = new Scanner(System.in);
-        luggageInfo info=new luggageInfo("Nikita",1,11,1,15);
-	    luggageBranch array=new luggageBranch(1);
-        array.setLuggageAt(0,info);
+        LuggageInfo info = new LuggageInfo("Nikita", 1, 11, 1, 15);
+        LuggageBranch array = new LuggageBranch(1);
+        array.setLuggageAt(0, info);
         array.resize(5);
-        array.setLuggageAt(1,new luggageInfo("Vitalik",2,22,2,25));
-        array.setLuggageAt(2,new luggageInfo("Valera",3,33,3,35));
-        array.setLuggageAt(3,new luggageInfo("Oleg",4,44,4,45));
-        array.setLuggageAt(4,new luggageInfo("Jora",5,55,5,65));
-        for (int i = 0; i <array.getArray().length ; i++) {
-            System.out.println(array.getArray()[i]);
-        }
+        array.setLuggageAt(1, new LuggageInfo("Vitalik", 2, 22, 2, 25));
+        array.setLuggageAt(2, new LuggageInfo("Valera", 3, 33, 3, 35));
+        array.setLuggageAt(3, new LuggageInfo("Oleg", 4, 44, 4, 45));
+        array.setLuggageAt(4, new LuggageInfo("Jora", 5, 55, 5, 65));
+
+        System.out.println(array);
+
         System.out.println("Luggage which weight more than 30kg");
-        for (int i = 0; i <array.getArray().length ; i++) {
-           if (array.getArray()[i].getLuggageWeight()>weight)
-            System.out.println(array.getArray()[i]);
+        for (int i = 0; i < array.getArray().length; i++) {
+            if (array.getArray()[i].getLuggageWeight() > weight)
+                System.out.println(array.getArray()[i]);
         }
-        System.out.println("Overall luggage weight ="+ array.getWeightSum());
+        System.out.println("Overall luggage weight =" + array.getWeightSum());
+        boolean next = true;
         do {
-            System.out.println("Please enter a luggage Check Number : ");
-            while (!in.hasNextInt()) {
-                System.out.println("That not a number!");
-                in.next();
+            try {
+                System.out.println("Please enter a luggage Check Number: ");
+                String number = in.nextLine();
+                n = Integer.parseInt(number);
+                if (n > 0)
+                    next = false;
+            } catch (InputMismatchException exc) {
+                System.out.println(exc.getLocalizedMessage());
+            } catch (Exception exc) {
+                System.out.println(exc.getLocalizedMessage());
             }
-            n=(in.nextInt());
-        } while (n <= 0 );
-        for (int i = 0; i <array.getArray().length ; i++) {
+        } while (next);
+
+        for (int i = 0; i < array.getArray().length; i++) {
             if (array.getArray()[i].getLuggageCheckNumber() == n) {
                 System.out.println("Flight number : " + array.getArray()[i].getFlightNumber());
             }
         }
+
+        System.out.println("\n----------------- Before removing ---------------------");
+        System.out.println(array);
+        System.out.println(array.remove(10));
+        System.out.println("\n------------------After removing --------------------");
+        System.out.println(array);
     }
 }
